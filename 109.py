af710a4615aae1dd01e5fa7d8ea0ba74473e70ba
Felix Peng <fpeng@g.hmc.edu>
2025-02-03 18:59:14 -0800

    109 complete, DFS and backtracking

--- 109.py
+++ 109.py
@@ -14,3 +14,25 @@ Return [[1, 2], [1, 3, 4], [1, 3, 5]].
 """
 
 #dfs?
+
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def binary_tree_path(root):
+    def dfs(node, path, result):
+        if not node:
+            return None
+        path.append(node.value)
+        if node.left == None and node.right == None:
+            result.append(list(path))
+        else:
+            dfs(node.left, path, result)
+            dfs(node.right, path result)
+        path.pop()
+
+    result = []
+    dfs(root, [], result)
+    return result
