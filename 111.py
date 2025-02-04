a4a06bd055f7e8c6e6fa2251cf89e7f6b4c57316
Felix Peng <fpeng@g.hmc.edu>
2025-02-04 11:20:02 -0800

    111, Leetcode hard with anagrams

---
+++ 111.py
@@ -0,0 +1,8 @@
+"""
+Given a word W and a string S, find all starting indices in S which are anagrams of W.
+
+For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
+"""
+#hard
+#check for anagrams usually using a hashmap
+#start with helper function of anagram checker
