f22ea182ba28b56e6d5b309b727c119cb1ec1dca
Felix Peng <fpeng@g.hmc.edu>
2025-02-09 15:20:24 -0800

    Completed 108, shiftable, in place shifting

--- 108.py
+++ 108.py
@@ -11,4 +11,34 @@ For example, if A is abcde and B is cdeab, return true. If A is abc and B is acb
 
 def shiftable(a, b):
 
+    def shift(a, k):
+        # shift a by k
+        # abcde, k=2, deabc ----- double reverse/in place?
+        a = list(a)
+        l, r = 0, len(a)-1
+        while l<r:
+            a[l], a[r] = a[r], a[l]
+            l += 1
+            r -= 1
+            # edcba
+        l, r = 0, k-1
+        while l<r:
+            a[l], a[r] = a[r], a[l]
+            l += 1
+            r -= 1
+        l, r = k, len(a)-1
+        while l<r:
+            a[l], a[r] = a[r], a[l]
+            l += 1
+            r -= 1
+        return "".join(a)
+    
+    for k in range(len(a)):
+        if shift(a, k) == b:
+            return True
+    
+    return False
 
+a = "abcde"
+b = "cdab"
+print(shiftable(a,b))
