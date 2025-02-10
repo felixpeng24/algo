392d5fadf2895a695e197d030ccabb6c22d38698
Felix Peng <fpeng@g.hmc.edu>
2025-02-10 11:45:24 -0800

    106, greedy

--- 106.py
+++ 106.py
@@ -6,3 +6,13 @@ For example, [2, 0, 1, 0] returns True while [1, 1, 0, 1] returns False.
 """
 
 # brute force solution? could ask more clarifying qs like if the numbers stack
+
+def reachCheckpoint(nums):
+    fuel = 0
+    for num in nums:
+        if fuel < 0:
+            return False
+        else:
+            fuel -= 1
+            fuel += num
+    return True
