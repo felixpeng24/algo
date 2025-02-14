c682910339ce68d49a59dedc67d828768b17c6f1
Felix Peng <fpeng@g.hmc.edu>
2025-02-13 19:13:28 -0800

    Greedy algo notes

--- 119.py
+++ 119.py
@@ -3,3 +3,5 @@ Given a set of closed intervals, find the smallest set of numbers that covers al
 
 For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers that covers all these intervals is {3, 6}.
 """
+
+# use greedy, last number of first list, keep adding until its out of range (need to sort first by using lambda of second element of list)
