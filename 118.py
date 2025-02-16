66dfd145dd63f39b95220294811d16df5d52eca5
Felix Peng <fpeng@g.hmc.edu>
2025-02-16 11:10:52 -0800

    122 and 123 + Initial thoughts

--- 118.py
+++ 118.py
@@ -1,4 +1,4 @@
-4"""
+"""
 Given a sorted list of integers, square the elements and give the output in sorted order.
 
 For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
@@ -11,20 +11,22 @@ def sqrsort(nums):
     nums.sort()
     return nums
 
-"""
-doesnt work since num doesnt change the actual value inside of nums
 
+# doesnt work since num doesnt change the actual value inside of nums
+
+
+"""
 def sqrsort(nums):
     for num in nums:
         num = num**2
     nums.sort()
     return nums
 
-"""
+
 nums = [-9, -2, 0, 2, 3]
  #print(sqrsort(nums))
 
-"""
+
 def sqrsortptr(nums):
     ptr = 0
     while nums[ptr] < 0:
---
+++ 123.py
@@ -0,0 +1,24 @@
+"""
+
+Given a string, return whether it represents a number. Here are the different kinds of numbers:
+
+"10", a positive integer
+"-10", a negative integer
+"10.1", a positive real number
+"-10.1", a negative real number
+"1e5", a number in scientific notation
+And here are examples of non-numbers:
+
+"a"
+"x 1"
+"a -2"
+"-"
+
+"""
+
+#also doesnt seem hard which scares me...
+#check for neg in front
+#check for multiple occurences of e, .
+# check for anything other than numbers and those mentioned above
+# no decimals after e
+#cant start with decimal?
