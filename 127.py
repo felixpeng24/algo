34cc45a4a1609771e8f138141305812ea3841c46
Felix Peng <fpeng@g.hmc.edu>
2025-02-20 14:23:32 -0800

    Completed 127 Easy, Linkedlist to number + Dummy Node

--- 127.py
+++ 127.py
@@ -18,3 +18,43 @@ return 124 (99 + 25) as:
 
 
 """
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def linked_list_to_number(head):
+    num = 0
+    multiplier = 1
+    while head:
+        num += head.val * multiplier
+        multiplier *= 10
+        head = head.next
+    return num
+
+def number_to_linked_list(num):
+    dummy = ListNode()
+    current = dummy
+    for digit in str(num)[::-1]:  # Reverse iteration
+        current.next = ListNode(int(digit))
+        current = current.next
+    return dummy.next
+
+def add_linked_lists(l1, l2):
+    num1 = linked_list_to_number(l1)
+    num2 = linked_list_to_number(l2)
+    total = num1 + num2
+    return number_to_linked_list(total)
+
+
+
+
+def num_to_linked(num):
+    dummy = ListNode()
+    curr = dummy
+    for digit in str(num)[::-1]:
+        curr.next = ListNode(int(digit))
+        curr = curr.next
+    return dummy.next
+
