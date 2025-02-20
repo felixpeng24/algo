5381febd03cc763ccd291de826a30568d71a4bd7
Felix Peng <fpeng@g.hmc.edu>
2025-02-20 11:17:08 -0800

    127 Easy Linkedlists

--- 121.py
+++ 121.py
@@ -3,3 +3,10 @@ Given a string which we can delete at most k, return whether you can make a pali
 
 For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
 """
+
+#pointer at the ends
+
+def is_removable_palindrome(s, k):
+    l, r = 0, len(s)-1
+    # for the example of like "qqqqqqracecar"
+    if s[l] != s[r]:
---
+++ 127.py
@@ -0,0 +1,20 @@
+"""
+Easy
+
+For example, the following linked list:
+
+1 -> 2 -> 3 -> 4 -> 5
+is the number 54321.
+
+Given two linked lists in this format, return their sum in the same linked list format.
+
+For example, given
+
+9 -> 9
+5 -> 2
+return 124 (99 + 25) as:
+
+4 -> 2 -> 1
+
+
+"""
