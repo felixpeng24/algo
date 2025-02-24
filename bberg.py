5238cd9b941837eec79a9a49314e9372381e1086
Felix Peng <fpeng@g.hmc.edu>
2025-02-24 14:43:01 -0800

    SUbstring Generation using iteration and recursion

--- bberg.py
+++ bberg.py
@@ -12,3 +12,23 @@ bc
 ac
 abc
 """
+
+def string_output(s):
+    for l in range(len(s)):
+        for i in range(len(s)):
+            print(s[l:i+1])
+
+
+def generate_subsets(s, index=0, current=""):
+    if index == len(s):
+        print(current)
+        return
+    
+    # Exclude the current character
+    generate_subsets(s, index + 1, current)
+    
+    # Include the current character
+    generate_subsets(s, index + 1, current + s[index])
+
+# Example usage
+generate_subsets("abc")
