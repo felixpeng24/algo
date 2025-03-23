555d81a865251de2d597bcc16cf5ba43a2fe5c19
Felix Peng <fpeng@g.hmc.edu>
2025-03-23 10:56:43 -0700

    157 Easy, palindrome checker and rewriter

---
+++ 157.py
@@ -0,0 +1,9 @@
+"""
+Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, since it can be rearranged to form racecar, which is a palindrome. 
+daily should return false, since there's no rearrangement that can form a palindrome.
+
+"""
+
+# easy? check if it has the right combo of letters through hasmap
