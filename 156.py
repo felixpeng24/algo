433512d00da56e350f766a34068daad872372307
Felix Peng <fpeng@g.hmc.edu>
2025-03-29 11:06:08 -0700

    156 Completed, DP finds all vals in arr

--- 156.py
+++ 156.py
@@ -8,3 +8,20 @@ Given n = 27, return 3 since 27 = 3^2 + 3^2 + 3^2 = 9 + 9 + 9.
 """
 
 # go from biggest squarev value that is still equvalent, subtract, and go down the list of combinations since you want the smallest list?
+
+import math
+
+def min_squares(n):
+    dp = [float('inf')] * (n + 1)
+    dp[0] = 0  # Base case: 0 can be represented with 0 squares
+    
+    for i in range(1, n + 1):
+        j = 1
+        while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+            j += 1
+            
+    return dp
+
+# Example cases
+print(min_squares(13))  # Output: 2
