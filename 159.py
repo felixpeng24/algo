2db90af01b2204c36b1b887a87a1e6f630609290
Felix Peng <fpeng@g.hmc.edu>
2025-03-30 13:31:01 -0700

    Easy Hash set q

--- 159.py
+++ 159.py
@@ -6,3 +6,15 @@ For example, given the string "acbbac", return "b". Given the string "abcdef", r
 """
 
 # hash set
+
+def first_recurring_char(s):
+    seen = set()
+    for char in s:
+        if char in seen:
+            return char  # First recurring character found
+        seen.add(char)
+    return None  # No recurring character
+
+# Example usage:
+print(first_recurring_char("acbbac"))  # Output: "b"
+print(first_recurring_char("abcdef"))  # Output: None
