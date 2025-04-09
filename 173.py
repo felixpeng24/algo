cc769d6ebb5fe0a909e392f668dc2ee10a3b2730
Felix Peng <fpeng@g.hmc.edu>
2025-04-09 00:29:29 -0700

    173 DFS of dictionary

---
+++ 173.py
@@ -0,0 +1,34 @@
+"""
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+"""
+
+#dfs
+def flatten_dict(d, parent_key='', sep='.'):
+    items = {}
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.update(flatten_dict(v, new_key, sep=sep))
+        else:
+            items[new_key] = v
+    return items
