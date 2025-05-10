1066b5577877116a9e38513c2d9d338fcae7230d
Felix Peng <fpeng@g.hmc.edu>
2025-05-09 21:00:24 -0700

    DP with triangles

--- 201.py
+++ 201.py
@@ -16,3 +16,20 @@ Write a program that returns the weight of the maximum weight path.
 
 #or dp? lemme think how this would work, backtrakcing from bottom up
 
+def max_path_sum(triangle):
+    # Copy the last row as the initial state of our dp array
+    dp = triangle[-1][:]
+    
+    # Start from the second-to-last row, moving upward
+    for row in reversed(triangle[:-1]):
+        for i in range(len(row)):
+            # Update dp[i] to be the max path sum at that position
+            dp[i] = row[i] + max(dp[i], dp[i + 1])
+    
+    return dp[0]
+
+# Example usage
+triangle = [[1], [2, 3], [1, 5, 1]]
+print(max_path_sum(triangle))  # Output: 9
+
+#we were close
