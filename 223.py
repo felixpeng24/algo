b82d59518636bacbdd74560f98009fbd2f4ce8ac
Felix Peng <fpeng@g.hmc.edu>
2025-05-29 09:13:23 -0400

    223, Morris in order traversal

---
+++ 223.py
@@ -0,0 +1,7 @@
+"""
+Typically, an implementation of in-order traversal of a binary tree has O(h) space complexity, 
+where h is the height of the tree. Write a program to compute the in-order traversal of a binary tree using O(1) space.
+
+"""
+
+#create pointer of right of in order predecessor to the curr node, as a pointer for the order
