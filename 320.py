8cab9a64e6779bf140e3c22da6b62ea01a152488
felixpeng24 <fpeng@g.hmc.edu>
2025-09-12 02:16:56 -0700

    New problems

---
+++ 320.py
@@ -0,0 +1,8 @@
+"""
+Given a string, find the length of the smallest window that contains every distinct character. Characters may appear more than once in the window.
+
+For example, given "jiujitsu", you should return 5, corresponding to the final five letters.
+
+medium
+
+"""
